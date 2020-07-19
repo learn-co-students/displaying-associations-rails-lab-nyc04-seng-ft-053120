8c274cf8246c67bbbf5730e40ac0be2447102b93
vuonga1103 <vuonga1103@yahoo.com>
2020-07-18 20:43:02 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def song_and_artist
+    "#{self.artist.name} - #{self.title}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Artist.destroy_all
+
+a1 = Artist.create(name: "Daughter")
+a2 = Artist.create(name: "Selena Gomez")
+a3 = Artist.create(name: "Lights")
+a4 = Artist.create(name: "Adele")
+a5 = Artist.create(name: "Troye Sivan")
+
+s1 = Song.create(title: "Medicine", artist: a1)
+s2 = Song.create(title: "Back To You", artist: a2)
+s3 = Song.create(title: "Drive My Soul", artist: a3)
+s4 = Song.create(title: "Million Years Ago", artist: a4)
+s5 = Song.create(title: "Take Yourself Home", artist: a5)
+
+puts "All done seeding! :)"
