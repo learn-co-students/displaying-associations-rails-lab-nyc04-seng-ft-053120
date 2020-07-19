9f8c0b747bbf1bafa27d99cf4cdb3a8f0d704496
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-19 14:42:49 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+artists = Artist.create([
+  {
+    name: "Paul"
+  },
+  {
+    name: "Teresa"
+  },
+  {
+    name: "Sarah"
+  },
+  {
+    name: "Jessica"
+  },
+  {
+    name: "Ladybug"
+  }
+])
+
+songs = Song.create([
+  {
+    title: "Goose",
+    artist_id: 1
+  },
+  {
+    title: "Psych",
+    artist_id: 1
+  },
+  {
+    title: "Apples",
+    artist_id: 2
+  },
+  {
+    title: "Banana",
+    artist_id: 3
+  }
+])
