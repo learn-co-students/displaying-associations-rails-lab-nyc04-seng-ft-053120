07a6535a0c1870aabd86cc408eca4c01ba974e45
guillenjs <jorgeguilln@gmail.com>
2020-07-21 07:54:36 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,14 @@
 class ArtistsController < ApplicationController
   def index
+    @artist = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def song_count
+    self.count
   end
 
   def new
