f3a31c2f33aad15644628e2fdc2005a0a2424391
Valkryn <calvinraphaelg@gmail.com>
2020-07-21 18:44:43 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+puts "injecting!"
+Artist.create([{
+              name: "Linkin Park"
+              },
+              {
+              name: "Eminem"
+              },
+              {
+              name: "Uncle Kracker"
+              }
+              ])
+Song.create([{
+              title:"In the end",
+              artist_id:1
+            },
+            {
+              title: "Numb",
+              artist_id:1
+            },
+            {
+              title:"the way i am",
+              artist_id:2
+            },
+            {
+              title:"Loose yourself",
+              artist_id:2
+            },
+            {
+              title: "Follow me",
+              artist_id:3
+            },
+            {
+              title: "Drift away",
+              artist_id:3
+            }
+            ])
+  puts "Done!"
